4649ddf4107cdddef10672e8c9651b77841ade9d
Carolina Vera <cvera.nahuelcoy@gmail.com>
2023-05-11 22:46:02 -0400

    se crean archivos MDlinks.js y funciones.js para organizar codigo. se valida si ruta existe

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = () => {
+/* module.exports = () => {
   // ...
 };
+ */
+const fs = require('fs')
+
+fs.readFile('prueba.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+}); 
